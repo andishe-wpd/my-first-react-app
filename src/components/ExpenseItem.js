8e236dfa533bcd0e3e897e6e37fef453217a5f5d
andishe <rainbowboxdash@gmail.com>
2022-02-08 13:17:50 +0330

    Outputting Dynamic Data & Working with Expressions in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,16 +3,20 @@ import "./ExpenseItem.css";
 //father : app.js
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "care insurance";
+  const expenseAmounr = 298.2;
   // return need () if the return parameter is more than 1 line
   return (
     // must all JSX be in one single <div>
     // why? (explain later)
     // here is a JS ENVOIRNMENT not HTML so use className instead class
     <div className="expense-item">
-      <div>Date</div>
+      {/* date cant's use directly it must change to string() */}
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmounr}</div>
       </div>
     </div>
   );
