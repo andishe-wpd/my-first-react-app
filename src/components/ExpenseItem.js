8978d23ee38d2397c60161a6d1c90f8def5cf915
andishe <rainbowboxdash@gmail.com>
2022-02-08 12:37:14 +0330

    Writing More Complex JSX Code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 //father : app.js
 
 const ExpenseItem = () => {
-  return <h1> first react component</h1>;
+  // return need () if the return parameter is more than 1 line
+  return (
+    // must all JSX be in one single <div>
+    // why? (explain later)
+    <div>
+      <div>Date</div>
+      <h2>Title</h2>
+      <div>Amount</div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
