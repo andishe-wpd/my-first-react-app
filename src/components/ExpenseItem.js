f0ea75c78388e4700d2e6c82129bd9d02fc0d5f7
andishe <rainbowboxdash@gmail.com>
2022-02-08 13:50:08 +0330

    Passing Data via _props_

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,7 @@ import React from "react";
 import "./ExpenseItem.css";
 //father : app.js
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "care insurance";
-  const expenseAmounr = 298.2;
+const ExpenseItem = (props) => {
   // return need () if the return parameter is more than 1 line
   return (
     // must all JSX be in one single <div>
@@ -13,10 +10,10 @@ const ExpenseItem = () => {
     // here is a JS ENVOIRNMENT not HTML so use className instead class
     <div className="expense-item">
       {/* date cant's use directly it must change to string() */}
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmounr}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
