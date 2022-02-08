a62eadddfd96aecab760e29236edc4b66de82d83
andishe <rainbowboxdash@gmail.com>
2022-02-08 12:48:36 +0330

    Adding Basic CSS Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 //father : app.js
 
 const ExpenseItem = () => {
@@ -7,10 +7,13 @@ const ExpenseItem = () => {
   return (
     // must all JSX be in one single <div>
     // why? (explain later)
-    <div>
+    // here is a JS ENVOIRNMENT not HTML so use className instead class
+    <div className="expense-item">
       <div>Date</div>
-      <h2>Title</h2>
-      <div>Amount</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
     </div>
   );
 };
